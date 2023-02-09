58fb2e3ca7ef127e5e3394a61683aa177bf31dd7
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-09 16:13:16 -0500

    Dona http finalizado

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
-  styleUrls: ['./dona-http.component.scss']
+  styleUrls: ['./dona-http.component.scss'],
 })
 export class DonaHttpComponent implements OnInit {
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: [],
+    datasets: [
+      {
+        /* data: [350, 450, 100], */
+        data: [],
+        backgroundColor: ['#6405F0', '#0724E3', '#05A0F0'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
+  constructor(private grapicasServices: GraficasService) {}
 
   ngOnInit(): void {
+    this.grapicasServices
+      .getUsuariosDataDona()
+      .subscribe(({ labels, values }) => {
+        this.doughnutChartData = {
+          labels: labels,
+          datasets: [
+            {
+              data: values,
+            },
+          ],
+        };
+      });
   }
 
+  get data() {
+    return this.doughnutChartData.datasets[0].data;
+  }
 }
--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, delay, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GraficasService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getUsuariosRedesSociales(): Observable<any> {
+    return this.http.get<any>('http://localhost:3000/grafica');
+  }
+
+  getUsuariosDataDona(): Observable<{ labels: string[]; values: number[] }> {
+    return this.getUsuariosRedesSociales().pipe(
+      delay(1500),
+      map((data) => {
+        const labels = Object.keys(data);
+        const values = Object.values(data) as number[];
+        return { labels, values };
+      })
+    );
+  }
 }
