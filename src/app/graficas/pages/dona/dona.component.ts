b5e623d4cfecdd738914088e3713860fa1472ac8
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-09 15:41:11 -0500

    Grafico de dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js/dist/types/index';
 
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html',
-  styleUrls: ['./dona.component.scss']
+  styleUrls: ['./dona.component.scss'],
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#6405F0', '#0724E3', '#05A0F0'],
+      },
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
